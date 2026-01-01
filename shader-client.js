eef8d2aeea47292483de85c61c451b8c1dad1fe5
Ruslan Osipov <rusya13@gmail.com>
2026-01-01 21:46:24 +1100

    Update shader-client.js

--- shader-client.js
+++ shader-client.js
@@ -147,7 +147,7 @@ class ShaderClient {
       const workerUrl = new URL(this.options.workerPath + '?v=' + Date.now(), location.href).href
       const connection = new window.BareMux.BareMuxConnection(workerUrl)
       const wispURL = window.__uv$config.wisp
-      const transportUrl = new URL('vector/index.mjs?v=' + Date.now(), location.href).href
+      const transportUrl = new URL('vector/index.js', location.href).href
 
       console.log('🔧 Setting transport to Wisp (Remote Server):', wispURL)
       await connection.setTransport(transportUrl, [{ wisp: wispURL }])
