067f214807c03ead4dc1b1491605bef438cb2c4b
Ruslan Osipov <rusya13@gmail.com>
2026-01-02 16:17:55 +1100

    Refactor compute.js to use static BareMux import
    Replaces dynamic import of BareMux with a static import and updates references accordingly. Also updates usage of global variables to use 'self' for consistency and removes the async init function, streamlining initialization.

--- compute.js
+++ compute.js
@@ -1,40 +1,36 @@
+import * as BareMux from './matrix/index.js'
 import './shader.bundle.js'
 import './shader.config.js'
 import './shader.kernel.js'
 
-async function init() {
-  const BareMux = await import('./matrix/index.js')
-  self.BareMux = BareMux
+self.BareMux = BareMux
 
-  const uv = new UVServiceWorker()
-  const connection = new BareMux.BareMuxConnection('./matrix/worker.js?raw=true')
+const uv = new self.UVServiceWorker()
+const connection = new BareMux.BareMuxConnection('./matrix/worker.js?raw=true')
 
-  let transportReady = false
+let transportReady = false
 
-  async function setupTransport() {
-    const wispUrl = __uv$config.wisp
+async function setupTransport() {
+  const wispUrl = self.__uv$config.wisp
 
-    try {
-      await connection.setTransport('./vector/index.mjs', [{ wisp: wispUrl }])
-      console.log('[SW] Vector transport configured (Remote):', wispUrl)
-      transportReady = true
-    } catch (err) {
-      console.error('[SW] Failed to set Wisp transport:', err)
-    }
+  try {
+    await connection.setTransport('./vector/index.mjs', [{ wisp: wispUrl }])
+    console.log('[SW] Vector transport configured (Remote):', wispUrl)
+    transportReady = true
+  } catch (err) {
+    console.error('[SW] Failed to set Wisp transport:', err)
   }
-
-  const transportPromise = setupTransport()
-
-  self.addEventListener('fetch', (event) => {
-    if (event.request.url.startsWith(location.origin + __uv$config.prefix)) {
-      event.respondWith(
-        (async () => {
-          await transportPromise
-          return await uv.fetch(event)
-        })()
-      )
-    }
-  })
 }
 
-init()
+const transportPromise = setupTransport()
+
+self.addEventListener('fetch', (event) => {
+  if (event.request.url.startsWith(location.origin + self.__uv$config.prefix)) {
+    event.respondWith(
+      (async () => {
+        await transportPromise
+        return await uv.fetch(event)
+      })()
+    )
+  }
+})
